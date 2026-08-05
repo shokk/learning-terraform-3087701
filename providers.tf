4ce43446a40a9edd5d0caa5c9fe0bf12927b2e6c
Ernie Oporto <ernieoporto@yahoo.com>
2026-08-05 16:36:10 -0400

    required_version linted

--- providers.tf
+++ providers.tf
@@ -3,6 +3,7 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.30.0" # Allows only patch updates (e.g., 5.30.1, 5.30.2)
+      required_version = ">= 1.0"
     }
   }
 }
