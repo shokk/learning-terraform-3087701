e4a0f103f82ab6a0fb0d81f9c9716cd3264577dd
Ernie Oporto <ernieoporto@yahoo.com>
2026-08-05 15:32:06 -0400

    fix syntax

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-vpc_security_group_ids = [module.blog_sg.security.group.id]
+vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
