09c53329bb9748f94fb80a20dbe845388daeadf6
Ernie Oporto <ernieoporto@yahoo.com>
2026-08-05 15:34:44 -0400

    fixed location of ingress and egress lines

--- main.tf
+++ main.tf
@@ -44,6 +44,12 @@ module "blog_sg" {
   name    = "blog"
 
   vpc_id = data.aws_vpc.default.id
+
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog" {
@@ -51,12 +57,6 @@ resource "aws_security_group" "blog" {
   description = "Allow http and https in. Allow everything out."
 
   vpc_id = data.aws_vpc.default.id
-
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules       = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
