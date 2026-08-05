5599ec7729a5673f96b5d891a312095a3aa55ad3
Ernie Oporto <ernieoporto@yahoo.com>
2026-08-05 17:04:54 -0400

    load balancer

--- main.tf
+++ main.tf
@@ -75,3 +75,40 @@ module "blog_sg" {
 output "public_ip" {
   value = aws_instance.blog.public_ip
 }
+
+module "blog_alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+
+  security_groups = [module.blog_sg.security_group_id]
+
+  listeners = {
+    blog-http = {
+      port     = 80
+      protocol = "HTTP"
+      forward = {
+        target_group_arn = aws_lb_target_group.blog.arn
+      }
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+resource "blog" "test" {
+  name     = "blog"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.blog_vpc.vpc_id
+}
+
+resource "aws_lb_target_group_attachment" "blog" {
+  target_group_arn = aws_lb_target_group.blog.arn
+  target_id        = aws_instance.blog.id
+  port             = 80
+}
