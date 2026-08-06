346252dc3bb7b060e39456081aac8783a5814a78
Ernie Oporto <ernieoporto@gmail.com>
2026-08-05 20:44:12 -0400

    variables file

--- variables.tf
+++ variables.tf
@@ -3,3 +3,39 @@ variable "instance_type" {
   default     = "t3.nano"
   type        = string
 }
+
+variable "ami_filter" {
+  description = "Name filter and owner for AMI"
+  type = object ({
+    name  = string
+    owner = string
+  })
+
+  default = {
+    name  = "al2023-ami-202*.*-x86_64"
+    owner = "amazon"
+  }
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type = object ({
+    name           = string
+    network_prefix = string
+  })
+  default = {
+    name = "dev"
+    network_prefix = "10.0"
+  }
+}
+
+variable "min_size" {
+  description = "Minimum number of instances in the ASG"
+  default = 1
+}
+
+variable "max_size" {
+  description = "Maximum number of instances in the ASG"
+  default = 2
+}
+
